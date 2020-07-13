7ee19a1d6ba62f1cb749e15e7683dc7eed85b41f
David Horstman <davidzilla27@gmail.com>
2020-07-13 15:19:56 -0400

    Implement satisfactory cross-folder import

--- path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def dotdot(path):
-    return os.path.dirname(path)
-
-
-def get_path():
-    return dotdot(os.path.realpath(__file__))
--- stack/stack.py
+++ stack/stack.py
@@ -10,6 +10,17 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# path
+import os
+import sys
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir+"/singly_linked_list")
+# pylint: disable=import-error
+from singly_linked_list import LinkedList  # nopep8
+
+print(LinkedList())
+
+
 class Stack:
     def __init__(self):
         self.size = 0
